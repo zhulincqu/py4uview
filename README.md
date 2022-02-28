8b9b32b0554167d091cae72467203b3cd0bc8122
Lin Zhu <lin.zhu@maxiv.lu.se>
2022-02-28 22:13:25 +0100

    add markups attribute and _read_markups method
    Only markers and arbitary cross section can be read out so far.

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 ## Introduction
 __Py4Uview__ provides a set of tools for images saved by Uivew, which is used by SPEELEEM in MAXPEEM to save the user's data in format of .dat file. Py4Uview can read the image and metadata from the file saved by Uview software.
+
+## Quick start
+### Install
+```
+python -m pip install py4uview
+```
+### import
+``` python
+>>> import py4uview
+```
--- py4uview/reader.py
+++ py4uview/reader.py
@@ -63,8 +63,11 @@ class FileReader():
     __slots__ = (
         "metadata",
         "data",
+        "markups",
+        "recipe",
         "_position",
-        "_header",
+        "_markups",
+        "_leemheader",
     )
 
     def __init__(self, filepath):
@@ -85,20 +88,20 @@ class FileReader():
         """
         # UNIT_DICT = ('', 'V', 'mA', 'A', '°C', ' K', 'mV', 'pA', 'nA', '\xb5A')
 
-        temp = self._header[self._position + 1:].split(b'\x00')[0]
+        temp = self._leemheader[self._position + 1:].split(b'\x00')[0]
         name = temp[:-1].decode('cp1252')
         if sys.version_info[0] > 2:
             unit_tag = int(chr(temp[-1]))
         else:
             unit_tag = int(temp[-1])
-        val = struct.unpack('<f', self._header[self._position + len(temp)
+        val = struct.unpack('<f', self._leemheader[self._position + len(temp)
                                                + 2:self._position + len(temp) + 6])[0]
         offset = len(temp) + 5  # length of entire field
         return name, UNIT_DICT[unit_tag], val, offset
 
-    def _read_imgheader(self):
+    def _read_leemheader(self):
 
-        b_iter = iter(self._header)
+        b_iter = iter(self._leemheader)
 
         # 235: COL, 236:Gauge 3, 237:PCH, 106:MCH
         # GAUGE_TAGS = [106, 235, 236, 237]
@@ -125,8 +128,8 @@ class FileReader():
             # Average Images = 255 : Sliding Averaging
             elif b == 104:
                 self.metadata['Camera Exposure'] = [
-                    struct.unpack('<f', self._header[self._position + 1:self._position + 5])[0], 's']
-                self.metadata['Average Images'] = self._header[self._position + 5]
+                    struct.unpack('<f', self._leemheader[self._position + 1:self._position + 5])[0], 's']
+                self.metadata['Average Images'] = self._leemheader[self._position + 5]
                 logging.info('\t{:>3}\t{:<18}\t{:g} {}'.format(
                     b, 'Camera Exposure:',
                     self.metadata['Camera Exposure'][0],
@@ -161,7 +164,7 @@ class FileReader():
 
             # Image Title
             elif b == 233:
-                temp = self._header[self._position + 1:].split(b'\x00')[0]
+                temp = self._leemheader[self._position + 1:].split(b'\x00')[0]
                 self.metadata['Image Title'] = temp.decode('cp1252')
                 logging.info('\t{:>3}\t{:<18}\t{}'.format(
                     b, 'Image Title:', self.metadata['Image Title']))
@@ -170,7 +173,7 @@ class FileReader():
             # MCP screen
             elif b == 243:
                 self.metadata['MCPscreen'] = [
-                    struct.unpack('<f', self._header[self._position + 1:self._position + 5])[0], 'V']
+                    struct.unpack('<f', self._leemheader[self._position + 1:self._position + 5])[0], 'V']
                 logging.info('\t{:>3}\t{:<18}\t{:g} {}'.format(
                     b, 'MCPscreen:', self.metadata['MCPscreen'][0], self.metadata['MCPscreen'][1]))
                 offset = 4
@@ -178,7 +181,7 @@ class FileReader():
             # MCP channel plate
             elif b == 244:
                 self.metadata['MCPchannelplate'] = [
-                    struct.unpack('<f', self._header[self._position + 1:self._position + 5])[0], 'V']
+                    struct.unpack('<f', self._leemheader[self._position + 1:self._position + 5])[0], 'V']
                 logging.info('\t{:>3}\t{:<18}\t{:g} {}'.format(
                     b, 'MCPchannelplate:', self.metadata['MCPchannelplate'][0],
                     self.metadata['MCPchannelplate'][1]))
@@ -187,9 +190,9 @@ class FileReader():
             # Micrometers(x,y)
             elif b == 100:
                 self.metadata['Mitutoyo X'] = \
-                    [struct.unpack('<f', self._header[self._position + 1:self._position + 5])[0], 'mm']
+                    [struct.unpack('<f', self._leemheader[self._position + 1:self._position + 5])[0], 'mm']
                 self.metadata['Mitutoyo Y'] = \
-                    [struct.unpack('<f', self._header[self._position + 5:self._position + 9])[0], 'mm']
+                    [struct.unpack('<f', self._leemheader[self._position + 5:self._position + 9])[0], 'mm']
                 logging.info('\t{:>3}\t{:<18}\t{:g} {}'.format(
                     b, 'Mitutoyo X:', self.metadata['Mitutoyo X'][0],
                     self.metadata['Mitutoyo X'][1]))
@@ -200,17 +203,17 @@ class FileReader():
 
                 # Mirror state
             elif b == 242:
-                self.metadata['MirrorState'] = self._header[self._position + 1]
+                self.metadata['MirrorState'] = self._leemheader[self._position + 1]
                 logging.info('\t{:>3}\t{:<18}\t{:g}'.format(b, 'MirrorState:', self.metadata['MirrorState']))
                 offset = 2
 
             # FOV
             elif b == 110:
-                temp = self._header[self._position + 1:].split(b'\x00')[0]
+                temp = self._leemheader[self._position + 1:].split(b'\x00')[0]
                 fov_str = temp.decode('cp1252')
                 self.metadata['FOV cal. factor'] = \
                     float(struct.unpack('<f',
-                                        self._header[self._position + len(temp) + 2: self._position + len(temp) + 6])[
+                                        self._leemheader[self._position + len(temp) + 2: self._position + len(temp) + 6])[
                               0])
 
                 # for LEED images
@@ -248,22 +251,22 @@ class FileReader():
             # FOV rotation from LEEM presets
             elif b == 113:
                 self.metadata['Rotation'] = \
-                    [struct.unpack('<f', self._header[self._position + 1:self._position + 5])[0], 'degree']
+                    [struct.unpack('<f', self._leemheader[self._position + 1:self._position + 5])[0], 'degree']
                 logging.info('\t{:>3}\t{:<18}\t{:g} {}'.format(
                     b, 'Rotation:', self.metadata['Rotation'][0], self.metadata['Rotation'][1]))
                 offset = 4
             # Spin up or down
             elif b == 240:
-                self.metadata['Spin up_down'] = self._header[self._position + 1]
+                self.metadata['Spin up_down'] = self._leemheader[self._position + 1]
                 logging.info('\t{:>3}\t{:<18}\t{:g}'.format(b, 'Spin up_down:', self.metadata['Spin up_down']))
                 offset = 2
 
             # Theta and Phi
             elif b == 239:
                 self.metadata['Theta'] = \
-                    [struct.unpack('<f', self._header[self._position + 1:self._position + 5])[0], 'degree']
+                    [struct.unpack('<f', self._leemheader[self._position + 1:self._position + 5])[0], 'degree']
                 self.metadata['Phi'] = \
-                    [struct.unpack('<f', self._header[self._position + 5:self._position + 9])[0], 'degree']
+                    [struct.unpack('<f', self._leemheader[self._position + 5:self._position + 9])[0], 'degree']
                 logging.info('\t{:>3}\t{:<18}\t{:g} {}'.format(
                     b, 'Theta:', self.metadata['Theta'][0],
                     self.metadata['Theta'][1]))
@@ -291,16 +294,67 @@ class FileReader():
             current_position(int): Number of position for the metadata's
                 name.
         """
-        temp_1 = self._header[self._position + 1:].split(b'\x00')[0]
-        temp_2 = self._header[self._position + 1:].split(b'\x00')[1]
+        temp_1 = self._leemheader[self._position + 1:].split(b'\x00')[0]
+        temp_2 = self._leemheader[self._position + 1:].split(b'\x00')[1]
         str_1 = temp_1.decode('cp1252')  # Name
         str_2 = temp_2.decode('cp1252')  # Unit
-        val = struct.unpack('<f', self._header[self._position + len(temp_1) + len(temp_2)
+        val = struct.unpack('<f', self._leemheader[self._position + len(temp_1) + len(temp_2)
                                                + 3: self._position + len(temp_1) + len(temp_2) + 7])[0]
         offset = len(temp_1) + len(temp_2) + 6  # length of entire field
-        logging.info('\t{:>3}\t{:<18}\t{:g} {}'.format(self._header[self._position],
+        logging.info('\t{:>3}\t{:<18}\t{:g} {}'.format(self._leemheader[self._position],
                                                        str_1 + ':', val, str_2))
         return str_1, str_2, val, offset
+
+    def _read_markups(self):
+        """ read out the markups"""
+        MARKER_LENGTH = { # byte
+            "marker": 24,
+            "horizontal": 0,
+            "vertical": 0,
+            "arbitrary":14,
+            "label": 0,
+        }
+        self.markups = {}
+        i = 4 
+        type_marker = struct.unpack("<H", self._markups[i:i+2])[0]
+        while type_marker:
+            if type_marker == 6: # markers
+                mytype = "marker"
+                length_byte = MARKER_LENGTH[mytype]
+                raw_markup = self._markups[i:i+length_byte]
+                unpack_format =  "<" + str(length_byte//2) + "H"
+                markup = struct.unpack(unpack_format, raw_markup)
+                mark_val = {
+                    "x":markup[1],
+                    "y":markup[2],
+                    "radius": markup[3],
+                }
+                if mytype in self.markups.keys():
+                    self.markups[mytype].append(mark_val)
+                else: 
+                    self.markups[mytype] = list()
+                    self.markups[mytype].append(mark_val)
+                i += MARKER_LENGTH[mytype]
+            elif type_marker == 3: # arbitrary cross section
+                mytype = "arbitrary"
+                length_byte = MARKER_LENGTH[mytype]
+                raw_markup = self._markups[i:i+length_byte]
+                unpack_format =  "<" + str(length_byte//2) + "H"
+                markup = struct.unpack(unpack_format, raw_markup)
+                mark_val = {
+                    "x0_y0":(markup[1], markup[2]),
+                    "x1_y1":(markup[3], markup[4]),   
+                }
+                if mytype in self.markups.keys():
+                    self.markups[mytype].append(mark_val)
+                else: 
+                    self.markups[mytype] = list()
+                    self.markups[mytype].append(mark_val)
+                i += MARKER_LENGTH[mytype]                
+            else:
+                print("Unknown marker")
+                break
+            type_marker = struct.unpack("<H", self._markups[i:i+2])[0]
 
     def _read(self, filepath):
         """Generic reader of UView .dat files."""
@@ -332,12 +386,12 @@ class FileReader():
 
             f.seek(56, 1)  # spare
 
-            # read recipe if there is one
+            # read recipe (fixed 128 bytes) if there is one
             if attachedRecipeSize:
-                self.metadata['recipe'] = f.read(attachedRecipeSize)
+                self._recipe = f.read(attachedRecipeSize)
                 f.seek(128 - attachedRecipeSize, 1)
 
-            # read first block of image header
+            # read first block of image header (fixed size 288bytes)
             self.metadata['isize'] = struct.unpack('<h', f.read(2))[0]
             self.metadata['iversion'] = struct.unpack('<h', f.read(2))[0]
             self.metadata['colorscale_low'] = struct.unpack('<h', f.read(2))[0]
@@ -352,26 +406,34 @@ class FileReader():
 
             f.seek(1, 1)  # spare
 
-            self.metadata['att_markupsize'] = struct.unpack('<h', f.read(2))[0]
+            att_markupsize = struct.unpack('<h', f.read(2))[0]
+            self.metadata["att_markupsize"] = att_markupsize
             self.metadata['spin'] = struct.unpack('<h', f.read(2))[0]
-            versleemdata = struct.unpack('<h', f.read(2))[0]
+            versleemdata = struct.unpack('<h', f.read(2))[0] # 28 bytes
+            # self.metadata["versleemdata"] = versleemdata
 
             logging.info('\tCOLLECTING META DATA:\t')
             # read second block of image header into byte sequence
-            #      -     usually block of 256 bytes
+            #      -     usually block of 288(fixed size) - 28(read already) bytes
             #     -     if too many metadata are stored, 388 empty bytes
             #        followed by number given in versleemdata
-            if versleemdata == 2:
-                header = f.read(256)
+            if versleemdata <= 2:
+                leemheader = f.read(240) # 240 bytes leemdata.
+                f.seek(20, 1) # spare
             else:
-                f.seek(388, 1)
-                header = f.read(versleemdata)
+                f.seek(260, 1)
+                if att_markupsize > 0: 
+                    self._markups = f.read(128 * (att_markupsize//128 + 1))
+                    self._read_markups()
+                leemheader = f.read(versleemdata)
 
             #### DEBUG ####
-            logging.debug('type(img_header) = {}'.format(type(header)))
+            logging.debug('type(img_leemheader) = {}'.format(type(leemheader)))
             ###############
-            self._header = header
-            self._read_imgheader()
+            self._leemheader = leemheader
+            self._read_leemheader()
+
+
 
             # Now read image data
             # Seek relative to the file's end (2)
