c8a4037376c60fa709fec7d84f1e2097e223b9e3
Lin Zhu <lin.zhu@maxiv.lu.se>
2022-03-08 11:07:39 +0100

    version 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="py4uview",
-    version="0.0.2",
+    version="0.0.3",
     author="Lin Zhu",
     author_email="lin.zhu@maxiv.lu.se",
     description="Data analysis package for MAXPEEM",
